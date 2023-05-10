b897bdf8b4b5a9f57c4da43baaace34a74822c02
oliiversion <olivxrkei101@gmail.com>
2023-05-10 13:14:35 -0400

    ok

--- okay/src/components/HelloWorld.vue
+++ okay/src/components/HelloWorld.vue
@@ -1,40 +1,93 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div id="app">
+    <div id="alert" v-if="alert">{{ alert }}</div>
+    <form @submit.prevent="loginWithPassword">
+      <label>
+        Email or username
+        <input type="text" v-model="emailOrUsername" />
+      </label>
+      <label>
+        Password
+        <input type="password" v-model="password" />
+      </label>
+      <button type="submit">Log in</button>
+    </form>
+
+    <p>or</p>
+
+    <button @click.prevent="loginWithSSO">Log in with Google</button>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
+<script>
+// Initialize Userfront
+Userfront.init('demo1234')
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+export default {
+  data() {
+    return {
+      emailOrUsername: '',
+      password: '',
+      alert: ''
+    }
+  },
+  methods: {
+    // Log in with the form's email/username and password
+    loginWithPassword() {
+      this.alert = ''
+      Userfront.login({
+        method: 'password',
+        emailOrUsername: this.emailOrUsername,
+        password: this.password
+      }).catch((error) => {
+        this.alert = error.message
+      })
+    },
+    // Log in with SSO (google)
+    loginWithSSO() {
+      Userfront.login({ method: 'google' })
+    }
   }
+}
+</script>
+
+<style scoped>
+button,
+input {
+  display: block;
+  margin-bottom: 10px;
+}
+
+#alert {
+  color: red;
+  margin-bottom: 10px;
+}
+</style>
+
+<style scoped>
+body {
+  background-color: #074407;
+  font-family: Calleo-Regular, sans-serif;
+}
+
+table {
+  background-color: #015c6a;
+  font-family: Calleo-Regular, sans-serif;
+  color: aliceblue;
+}
+
+canvas {
+  background-color: aliceblue;
+}
+
+button,
+input {
+  display: block;
+  margin-bottom: 10px;
+}
+
+#alert {
+  color: red;
+  margin-bottom: 10px;
 }
 </style>
